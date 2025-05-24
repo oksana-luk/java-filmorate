5ecc72c6f43102266bf20a44a2724ef9fa367226
Mikhail Ignatenko <ignatenkomikhail239@gmail.com>
2025-05-24 09:49:00 +0300

    Amendments

--- src/main/java/ru/yandex/practicum/filmorate/dto/director/DirectorDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/director/DirectorDto.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.dto.director;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;

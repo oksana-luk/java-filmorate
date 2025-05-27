862a963ac7d74964563cafea3b7eb670e35d0b63
Alexey Zinchenko <zizann86@yandex.ru>
2025-05-27 13:45:19 +0500

    в ветке Develop, в классе FeedService: внедрил зависимость UserRepository userRepository, для того чтобы реализовать проверку на валидность User(тест в Postman прошел на не валидный User)

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -1,27 +1,39 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dal.FeedRepository;
+import ru.yandex.practicum.filmorate.dal.UserRepository;
 import ru.yandex.practicum.filmorate.dto.event.EventDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.EventMapper;
 import ru.yandex.practicum.filmorate.model.Event;
 import ru.yandex.practicum.filmorate.model.EventOperation;
 import ru.yandex.practicum.filmorate.model.EventType;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class FeedService {
     private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
+
+    public FeedService(FeedRepository feedRepository, @Qualifier("userRepository") UserRepository userRepository) {
+        this.feedRepository = feedRepository;
+        this.userRepository = userRepository;
+    }
 
     public List<EventDto> getFeed(Long id) {
+        Optional<User> userOpt = userRepository.findUserById(id);
+        if (userOpt.isEmpty()) {
+            throw new NotFoundException("Пользователь с id " + id + " не найден");
+        }
         List<Event> events = feedRepository.getFeed(id);
         return events.stream()
                 .map(EventMapper::mapToEventDto)
